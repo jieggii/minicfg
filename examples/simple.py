cc1df360f38ab828933d862df37275b8d9cc552f
jieggii <jieggii@protonmail.com>
2024-08-09 12:05:27 +0300

    update examples/simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -3,8 +3,11 @@ from minicfg.field import Field
 from minicfg.caster import to_int
 
 class Env(Minicfg):
-    DATABASE_HOST = Field()
-    DATABASE_PORT = Field(caster=to_int)
+    # DATABASE HOST will be set to localhost if no value provided
+    DATABASE_HOST: str = Field(default="localhost")
+
+    # DATABASE_PORT value will be cast to int
+    DATABASE_PORT: int = Field(caster=to_int)
 
 
 env = Env()
@@ -13,8 +16,12 @@ env.populate()
 print(f"{env.DATABASE_HOST=}")
 print(f"{env.DATABASE_PORT=}")
 
-
-# Try running DATABASE_HOST=localhost DATABASE_PORT=5432 python simple.py
+# Try running DATABASE_HOST=127.0.0.1 DATABASE_PORT=5432 python simple.py
 # Output:
-# >>> env.DATABASE_HOST='localhost'
+# >>> env.DATABASE_HOST='127.0.0.1'
+# >>> env.DATABASE_PORT=5432
+
+# Or DATABASE_PORT=5432 python simple.py
+# Output:
+# >>> env.DATABASE_HOST='localhost'  # default value is used
 # >>> env.DATABASE_PORT=5432
